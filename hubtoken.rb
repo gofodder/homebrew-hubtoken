af84a8152c90d08218695c858ab9965278ed9db1
Jason Milkins <jasonm23@gmail.com>
2016-09-05 22:50:16 +0800

    fix download link / sha256

--- hubtoken.rb
+++ hubtoken.rb
@@ -1,8 +1,8 @@
 class Hubtoken < Formula
   desc "Manage Github personal access tokens from the CLI"
   homepage "https://github.com/gofodder/hubtoken"
-  url "https://github.com/gofodder/hubtoken/releases/download/v1.0.5/hubtoken-1.0.5-osx.tar.gz"
-  sha256 ""
+  url "https://github.com/gofodder/hubtoken/releases/download/1.0.4/hubtoken-1.0.4-osx.tar.gz"
+  sha256 "749217240d1b4431288eeadd95f54fd5736166a39b59355c061e3db089f30d76"
 
   def install
     bin.install Dir["hubtoken"]
