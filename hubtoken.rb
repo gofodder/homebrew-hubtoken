1f0169d61a13503280a044100821356d971a971c
Jason Milkins <jasonm23@gmail.com>
2016-09-05 23:13:30 +0800

    Fix formula - use go build

--- hubtoken.rb
+++ hubtoken.rb
@@ -1,10 +1,23 @@
+require "language/go"
+
 class Hubtoken < Formula
   desc "Manage Github personal access tokens from the CLI"
   homepage "https://github.com/gofodder/hubtoken"
-  url "https://github.com/gofodder/hubtoken/releases/download/1.0.4/hubtoken-1.0.4-osx.tar.gz"
-  sha256 "749217240d1b4431288eeadd95f54fd5736166a39b59355c061e3db089f30d76"
+  url "https://github.com/gofodder/hubtoken/archive/1.0.4.tar.gz"
+  sha256 "5079e51ca937d5370d3987d7247be974ea33b91d31cef4d4f6b34acd0acae399"
+  head "https://github.com/gofodder/hubtoken.git"
+
+  depends_on "go" => :build
+  depends_on "godep" => :build
 
   def install
-    bin.install Dir["hubtoken"]
+    ENV["GOPATH"] = buildpath
+    dir = buildpath/"src/github.com/gofodder/hubtoken/"
+    dir.install Dir["*"]
+    ln_s buildpath/"src", dir
+    cd dir do
+      system "godep", "restore"
+      system "go", "build", "-o", bin/"hubtoken", "main.go"
+    end
   end
 end
