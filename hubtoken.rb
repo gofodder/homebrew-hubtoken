f138569c2da83084a55a6fb57fb81a71e14457f5
Jason Milkins <jasonm23@gmail.com>
2016-09-05 23:17:21 +0800

    Fix SHA256

--- hubtoken.rb
+++ hubtoken.rb
@@ -4,7 +4,7 @@ class Hubtoken < Formula
   desc "Manage Github personal access tokens from the CLI"
   homepage "https://github.com/gofodder/hubtoken"
   url "https://github.com/gofodder/hubtoken/archive/1.0.4.tar.gz"
-  sha256 "5079e51ca937d5370d3987d7247be974ea33b91d31cef4d4f6b34acd0acae399"
+  sha256 "749217240d1b4431288eeadd95f54fd5736166a39b59355c061e3db089f30d76"
   head "https://github.com/gofodder/hubtoken.git"
 
   depends_on "go" => :build
